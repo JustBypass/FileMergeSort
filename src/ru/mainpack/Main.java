2355a1d9231a7bc2524a990dd298705e7db16e58
c2050122 <nikisazikov56@gmail.com>
2022-02-19 01:30:52 +0300

    All Work

--- src/ru/mainpack/Main.java
+++ src/ru/mainpack/Main.java
@@ -8,7 +8,9 @@ import ru.sort.common.Sorter;
 public class Main {
 
     public static void main(String[] args){
+        /// This is for debugging....we could us args of course
         String[]strings = {"-a","-s","C:\\Users\\Admin\\IdeaProjects\\newone\\docs\\output.txt","C:\\Users\\Admin\\IdeaProjects\\newone\\docs\\file.txt","C:\\Users\\Admin\\IdeaProjects\\newone\\docs\\file2.txt","C:\\Users\\Admin\\IdeaProjects\\newone\\docs\\file3.txt"};
+        /// .........................
         try{
             paramReader pRead = new paramReader(strings);
             Sorter sorter = pRead.getSorterType();
